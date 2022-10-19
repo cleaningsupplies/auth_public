adeb6c60dd5809e1bf09e3d780edcc92ae9ae158
steffi <stephanielindner@outlook.de>
2022-10-19 21:42:40 +0200

    base sign in ok i guess

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,8 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import React from 'react';
 import "./css/signin.css";
-import Card from 'react-bootstrap/Card';
-import Stack from 'react-bootstrap/Stack';
 
 export default function Signin() {
 
@@ -13,36 +11,46 @@ export default function Signin() {
 
   return (
     <div className='sign_container'>
-    
-        {/* <Card.Title>Please Log In</Card.Title> */}
         <Container fluid>
+            <Row className="justify-content-md-center mb-4">
+                <Col xs lg="7">
+                    <h1>Sign in.</h1>
+                    <p>Sign in to your account here.</p>
+                </Col>
+            </Row>
             <Row className="justify-content-md-center">
                 <Col xs lg="7">
                     <form>
-                        <div>
+                        <div className='mb-2'>
                             <label htmlFor="mail" className="label-text">Email</label>
-                            <input type="email" className="form-control" id="mail" placeholder="Enter your email"></input>
+                            <input type="email" className="form-control plh" id="mail" placeholder="Enter your email"></input>
                         </div>
-                        <div>
+                        <div className='mb-2'>
                             <label htmlFor="password" className="label-text">Password</label>
-                            <input type="password" className="form-control" id="password" placeholder="Enter your password"></input>
-                            <small>forgot password</small>
+                            <input type="password" className="form-control plh" id="password" placeholder="Enter your password"></input>
+                        </div>
+                        <div className='mb-3 forgot'>
+                            <a href='/forgot' className='link'>Forgot password</a>
                         </div>
                         <div className="d-grid gap-2">
-                            <Button variant="login">Login</Button>
+                            <Button className='btn-login'>Login</Button>
                         </div>
                     </form>
                 </Col>
             </Row>
+            {/* <Row className="justify-content-md-center">
+                <Col xs lg="7">
+                    <div className="d-grid gap-2">
+                        <Button variant="login">with google</Button>
+                    </div>
+                </Col>
+            </Row> */}
+            <Row className="justify-content-md-center mt-5">
+                <Col xs lg="7" className="signup">
+                    Don't have account yet?&nbsp;<a href="/signup" className='link'>Sign up</a>
+                </Col>
+            </Row>
         </Container>
-
-        <style type="text/css">{`
-            .btn-login {
-                background-color: #8b909a;
-                border: none;
-                color: white;
-            }
-        `}</style>
     </div>
   )
 }
